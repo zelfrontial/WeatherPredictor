a7240a19dc5ed772ddc759c99bb1e1069c0a5b07
zelfronital <denisthamrin@gmail.com>
2015-05-23 19:22:55 +1000

    create winds updated

--- db/migrate/20150523065721_create_winds.rb
+++ db/migrate/20150523065721_create_winds.rb
@@ -1,7 +1,7 @@
 class CreateWinds < ActiveRecord::Migration
   def change
     create_table :winds do |t|
-      t.String :windDirection
+      t.string :windDirection
       t.float :windSpeed
 
       t.timestamps null: false
