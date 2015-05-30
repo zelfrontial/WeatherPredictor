78a1e2ee8cee43dd7bb3fd091de87c84d5409f2b
zelfrontial <denisthamrin@gmail.com>
2015-05-31 00:34:34 +1000

    scraper with bug

--- lib/scraperbom.rb
+++ lib/scraperbom.rb
@@ -30,8 +30,9 @@ windspeed_list =  doc.css('tbody tr.rowleftcolumn td[headers*=obs-wind-spd-kph]'
 		sr.station_id = station.id
 	end
 	time = Time.strptime(time_list[row].content, "%e/%I:%M%P").in_time_zone("Melbourne")
+
+
 	single_reading = SingleReading.where(:time => time,:station_id => station.id)
-	
 	# if there is duplicate then break
 	if single_reading
 		break
@@ -40,10 +41,31 @@ windspeed_list =  doc.css('tbody tr.rowleftcolumn td[headers*=obs-wind-spd-kph]'
 
 
 	sr.time = time
-	sr.rainfall = ((rainfall_list[row].content.eql? '-') ? nil : rainfall_list[row].content.to_f)
-	sr.temperature = ((temp_list[row].content.eql? '-') ? nil : temp_list[row].content.to_f)
-	sr.dewPoint = ((dewpoint_list[row].content.eql? '-') ? nil : dewpoint_list[row].content.to_f)
-	sr.windSpeed = ((windspeed_list[row].content.eql? '-') ? nil : windspeed_list[row].content.to_f)
-	sr.windDirection = ((winddirection_list[row].content.eql? '-') ? nil : winddirection_list[row].content)
+
+
+
+	rain = ((rainfall_list[row].content.eql? '-') ? nil : rainfall_list[row].content.to_f)
+	rainfall = Rainfall.new(rainfall: rain)	
+	rainfall.save
+
+	tmp = ((temp_list[row].content.eql? '-') ? nil : temp_list[row].content.to_f)
+	temperature = Temperature.new(temperature: tmp)
+	temperature.save
+
+	dew_p = ((dewpoint_list[row].content.eql? '-') ? nil : dewpoint_list[row].content.to_f)
+	dewPoint = DewPoint.new(dewPoint: dew_p)
+	dewPoint.save
+
+
+	w_speed = ((windspeed_list[row].content.eql? '-') ? nil : windspeed_list[row].content.to_f)
+	w_dir = ((winddirection_list[row].content.eql? '-') ? nil : winddirection_list[row].content)
+	wind = Wind.new(windDirection: w_dir,windSpeed: w_speed)
+	wind.save
+
+	sr.dew_point_id = dewPoint.id
+	sr.rainfall_id = rainfall.id
+	sr.temperature_id = temperature.id
+	sr.wind_id = wind.id
+
 	sr.save
 end
